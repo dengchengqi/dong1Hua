c51ef614d187477ef50a3bdd0dd37370d3f7fd50
珂王 <ibokan@wangke.local>
2012-09-13 10:33:15 +0800

    Initial Commit

---
+++ dong1Hua/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dong1Hua
+//
+//  Created by Ibokan 王珂 on 12-9-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "text1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([text1AppDelegate class]));
+    }
+}
---
+++ dong1Hua/text1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  text1AppDelegate.h
+//  dong1Hua
+//
+//  Created by Ibokan 王珂 on 12-9-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface text1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
